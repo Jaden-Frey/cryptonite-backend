4819fe56126c9d37d35acb22947b03b587af08fb
Jaden Frey <FRYJAD001@myuct.ac.za>
2024-10-03 11:53:54 +0200

    Vercel Push

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://cryptonite-backend.vercel.app/'] 
+    ? ['https://cryptonite-backend-deploy.vercel.app/'] 
     : ['http://localhost:5001'],
   credentials: true,
 };
